0176e911bc59e385669614477176304a4be32817
gamoridev <breno.amorim@akna.com>
2020-10-15 19:56:23 -0300

    remove mute

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-require('dotenv').config()
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('voiceStateUpdate', async (oldState, newState) => {
+client.on("voiceStateUpdate", async (oldState, newState) => {
   if (oldState.channel !== newState.channel) {
-    if (oldState.channel && oldState.channel.name === 'General') {
-      play(oldState.channel, './sounds/disconnected.wav');
+    if (oldState.channel && oldState.channel.name === "Live") {
+      play(oldState.channel, "./sounds/disconnected.wav");
     }
 
-    if (newState.channel && newState.channel.name === 'General') {
+    if (newState.channel && newState.channel.name === "Live") {
       const havingAGoodTime = Math.random() <= 0.05;
-      const file = havingAGoodTime ? './sounds/connected-but-its-loud-af.mp3' : './sounds/connected.wav';
+      const file = havingAGoodTime
+        ? "./sounds/connected-but-its-loud-af.mp3"
+        : "./sounds/connected.wav";
       play(newState.channel, file);
     }
   }
 
-  if (oldState.selfDeaf && !newState.selfDeaf) {
-    play(oldState.channel, './sounds/talkpower_granted.wav');
-  }
-})
+  // if (oldState.selfDeaf && !newState.selfDeaf) {
+  //   play(oldState.channel, './sounds/talkpower_granted.wav');
+  // }
+});
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-async function play(channel, file)
-{
+async function play(channel, file) {
   const connection = await channel.join();
 
-  connection.play(file);
+  connection.play(file, { volume: 0.4 });
 }
